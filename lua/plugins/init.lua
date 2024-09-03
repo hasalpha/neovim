2b1cd859ad674d699d86d548f00c0bd4572e25e3
Mohammed Arshad Hassan <hasan.arshad18@gmail.com>
2024-09-03 14:37:24 +0530

    which key

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,62 +1,86 @@
 return {
 	checker = { enabled = true },
 	{
-		'nvim-treesitter/nvim-treesitter',
+		"nvim-treesitter/nvim-treesitter",
 		build = function()
-			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
 			ts_update()
 		end,
 	},
 
 	{
 		"windwp/nvim-autopairs",
-		config = function() require("nvim-autopairs").setup {} end
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
 	},
 
 	{
 		"windwp/nvim-ts-autotag",
-		config = function() require("nvim-ts-autotag").setup() end
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
 	},
 	"tpope/vim-fugitive",
 	{
-		'nvim-treesitter/nvim-treesitter-context',
-		config = function() require("treesitter-context").setup() end
+		"nvim-treesitter/nvim-treesitter-context",
+		config = function()
+			require("treesitter-context").setup()
+		end,
 	},
 
 	{
-		'nvim-lualine/lualine.nvim',
-		dependencies = { 'kyazdani42/nvim-web-devicons', opt = true }
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "kyazdani42/nvim-web-devicons", opt = true },
 	},
-	'nvim-tree/nvim-web-devicons',
+	"nvim-tree/nvim-web-devicons",
 	{
 		"folke/trouble.nvim",
 		dependencies = "kyazdani42/nvim-web-devicons",
 		config = function()
-			require("trouble").setup {}
-		end
+			require("trouble").setup({})
+		end,
 	},
-	'folke/lsp-colors.nvim',
-	'tjdevries/colorbuddy.nvim',
-	'f-person/git-blame.nvim',
-	'lukas-reineke/indent-blankline.nvim',
-	{ 'romgrk/barbar.nvim',      wants = 'nvim-web-devicons' },
-	'tpope/vim-surround',
+	"folke/lsp-colors.nvim",
+	"tjdevries/colorbuddy.nvim",
+	"f-person/git-blame.nvim",
+	"lukas-reineke/indent-blankline.nvim",
+	{ "romgrk/barbar.nvim", wants = "nvim-web-devicons" },
+	"tpope/vim-surround",
 	{
-		'numToStr/Comment.nvim',
+		"numToStr/Comment.nvim",
 		config = function()
-			require('Comment').setup()
-		end
+			require("Comment").setup()
+		end,
 	},
 	{ "ellisonleao/gruvbox.nvim" },
-	'NvChad/nvim-colorizer.lua',
-	'RRethy/vim-illuminate',
-	'yonlu/omni.vim',
-	'olimorris/onedarkpro.nvim',
-	{ 'stevearc/dressing.nvim' },
+	"NvChad/nvim-colorizer.lua",
+	"RRethy/vim-illuminate",
+	"yonlu/omni.vim",
+	"olimorris/onedarkpro.nvim",
+	{ "stevearc/dressing.nvim" },
 
-	'kevinhwang91/nvim-bqf',
+	"kevinhwang91/nvim-bqf",
 	{
 		"ray-x/lsp_signature.nvim",
 	},
-	'prettier/vim-prettier',
+	"prettier/vim-prettier",
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
 }
